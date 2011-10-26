6ae26c7ad4da42e0a6b8e9af08483508e0c7baa3
Mark Ottaviani <mark.ottaviani@gmail.com>
2011-10-25 22:50:58 -0400

    fleshed out dog form

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,23 +2,66 @@
 #
 # Table name: dogs
 #
-#  id               :integer         not null, primary key
-#  name             :string(255)
-#  created_at       :datetime
-#  updated_at       :datetime
-#  tracking_id      :integer
-#  breed_id         :integer
-#  age              :string(75)
-#  size             :string(75)
-#  is_altered       :boolean
-#  gender           :string(6)
-#  is_special_needs :boolean
-#  no_dogs          :boolean
-#  no_cats          :boolean
-#  no_kids          :boolean
-#  status           :string(255)
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  tracking_id        :integer
+#  primary_breed_id   :integer
+#  secondary_breed_id :integer
+#  status             :string(255)
+#  age                :string(75)
+#  size               :string(75)
+#  is_altered         :boolean
+#  gender             :string(6)
+#  is_special_needs   :boolean
+#  no_dogs            :boolean
+#  no_cats            :boolean
+#  no_kids            :boolean
+#  description        :text
 #
 
 class Dog < ActiveRecord::Base
+
+	attr_accessible :name, 
+					:tracking_id, 
+					:primary_breed_id,
+					:secondary_breed_id,
+					:status,
+					:age,
+					:size,
+					:is_altered,
+					:gender,
+					:is_special_needs,
+					:no_dogs,
+					:no_cats,
+					:no_kids,
+					:description
+
+	validates :name, :presence => true,
+					 :length   => { :maximum => 75 }
+
+	validates :tracking_id, :uniqueness => true,
+							:presence => true
+
+	validates_presence_of :primary_breed_id,
+						  :status,
+						  :age,
+						  :size,
+						  :gender
+
+	STATUSES = ['adoptable', 'adopted', 'adoption pending',
+				'hold', 'not available', 'return pending']
+	validates_inclusion_of :status, :in => STATUSES	
+
+	AGES = ['baby', 'young', 'adult', 'senior']		
+	validates_inclusion_of :age, :in => AGES	
+
+	SIZES = ['small', 'medium', 'large','extra large']	
+	validates_inclusion_of :size, :in => SIZES
+	
+	GENDERS = ['Male', 'Female']
+	validates_inclusion_of :gender, :in => GENDERS
+
 end
 
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -7,20 +7,22 @@ end
 #
 # Table name: dogs
 #
-#  id               :integer         not null, primary key
-#  name             :string(255)
-#  created_at       :datetime
-#  updated_at       :datetime
-#  tracking_id      :integer
-#  breed_id         :integer
-#  age              :string(75)
-#  size             :string(75)
-#  is_altered       :boolean
-#  gender           :string(6)
-#  is_special_needs :boolean
-#  no_dogs          :boolean
-#  no_cats          :boolean
-#  no_kids          :boolean
-#  status           :string(255)
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  tracking_id        :integer
+#  primary_breed_id   :integer
+#  secondary_breed_id :integer
+#  status             :string(255)
+#  age                :string(75)
+#  size               :string(75)
+#  is_altered         :boolean
+#  gender             :string(6)
+#  is_special_needs   :boolean
+#  no_dogs            :boolean
+#  no_cats            :boolean
+#  no_kids            :boolean
+#  description        :text
 #
 

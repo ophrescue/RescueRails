a4cbf7962de886e5c1a6410448da070e61fbe81f
Mark Ottaviani <mark.ottaviani@gmail.com>
2011-10-31 18:22:15 -0400

    add validation to photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,7 @@ class Photo < ActiveRecord::Base
 
 	has_attached_file :photo, :styles => { :original => "1024x1024>", :large => "640x640", :medium => "320x320>", :thumb => "100x100>" }
 
+	validates_attachment_presence :photo
+	validates_attachment_size :photo, :less_than => 5.megabytes
+	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/pjpeg']
 end

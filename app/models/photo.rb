d66c4d9bdfb58cf2a688aff404bbe3233f51cf4f
Mark Ottaviani <mark.ottaviani@gmail.com>
2011-10-31 10:52:48 -0300

    Added new sizes to photos, including resize of original on upload.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
 	belongs_to :dog
 
-	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :photo, :styles => { :original => "1024x1024>", :large => "640x640", :medium => "320x320>", :thumb => "100x100>" }
 
 end

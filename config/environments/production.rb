a66a2871c9283bdb34ba9f0b16c670fa11aee4b6
Mark Ottaviani <mark.ottaviani@gmail.com>
2011-12-05 19:47:06 -0500

    email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 RescueRails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  #Action Mailer Configuration
+  config.action_mailer.default_url_options = { :host => "173.255.234.200" }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 

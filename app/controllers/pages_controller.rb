f6b54f1bb27e263a336ee54eaf9f88009b8193b1
Mark Ottaviani <mark.ottaviani@gmail.com>
2011-12-08 10:40:55 -0500

    Added unique titles to static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,38 @@
 class PagesController < ApplicationController
   def home
-    @title = "Home"
+    @title = "All breed dog rescue based in Virgina and Maryland"
+  end
+  
+  def aboutus
+    @title = "About Us"
+  end
+  
+  def contributers
+    @title = "Contributers"
+  end
+  
+  def foster
+    @title = "Fostering Opportunities"
+  end
+  
+  def fosterfaq
+    @title = "Foster Frequently Asked Questions"
+  end
+  
+  def newsletter
+    @title = "Newsletter"
+  end
+  
+  def process
+    @title = "Adoption Process"
+  end
+  
+  def resources
+    @title = "Resources for Adopters"
+  end
+  
+  def volunteer
+    @title = "Volunteer Opportunities"
   end
 
 end

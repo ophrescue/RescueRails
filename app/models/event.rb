e232e582773b31f06f3f8aa3f6f2657ab75fae60
Mark Ottaviani <mark.ottaviani@gmail.com>
2011-12-12 19:44:46 -0500

    datetime fixes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,17 +1,35 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id              :integer         not null, primary key
+#  title           :string(255)
+#  location_name   :string(255)
+#  address         :string(255)
+#  description     :text
+#  created_by_user :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#  latitude        :float
+#  longitude       :float
+#  start_datetime  :datetime
+#  end_datetime    :datetime
+#
+
 class Event < ActiveRecord::Base
 
 	attr_accessible :title,
-				  				:start_time,
-				  				:end_time,
+				  				:start_datetime,
+				  				:end_datetime,
 				  				:location_name,
 				  				:address,
-				  				:descriptoin,
+				  				:description,
 									:latitude,
 									:longitude
 
 	validates_presence_of :title,
-											  :start_time,
-												:end_time,
+											  :start_datetime,
+												:end_datetime,
 												:location_name,
 												:address,
 												:description
@@ -30,22 +48,4 @@ class Event < ActiveRecord::Base
 	end
 
 end
-
-# == Schema Information
-#
-# Table name: events
-#
-#  id              :integer         not null, primary key
-#  title           :string(255)
-#  location_name   :string(255)
-#  address         :string(255)
-#  description     :text
-#  created_by_user :integer
-#  created_at      :datetime
-#  updated_at      :datetime
-#  latitude        :float
-#  longitude       :float
-#  start_datetime  :datetime
-#  end_datetime    :datetime
-#
 

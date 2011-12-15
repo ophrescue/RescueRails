d86310dc7d04cd22ffdbf21f146d790c18d5638d
Mark Ottaviani <mark.ottaviani@gmail.com>
2011-12-15 15:04:58 -0500

    async facebook loading

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -1,7 +1,17 @@
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+window.fbAsyncInit = function() {
+	FB.init({
+	  status     : true, // check login status
+	  cookie     : true, // enable cookies to allow the server to access the session
+	  xfbml      : true  // parse XFBML
+	});
+
+	// Additional initialization code here
+	};
+
+	// Load the SDK Asynchronously
+	(function(d){
+	 var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+	 js = d.createElement('script'); js.id = id; js.async = true;
+	 js.src = "//connect.facebook.net/en_US/all.js";
+	 d.getElementsByTagName('head')[0].appendChild(js);
+}(document));

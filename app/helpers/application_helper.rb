f1588f7c943f8f3eca585038b0e91a206d4825c9
Mark Ottaviani <mark.ottaviani@gmail.com>
2012-01-19 13:41:47 -0500

    finished sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,11 +16,8 @@ module ApplicationHelper
 
   def sortable(column, title = nil)
     title ||= column.titleize
-
-    
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    sort_updown = (sort_direction == "asc") ? "Up" : "Down"
-    css_class = (column == sort_column) ? "headerSort#{sort_updown}" : nil  
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
       

fe69a2f33662083d000ef7841c77c18019eef437
Mark Ottaviani <mark.ottaviani@gmail.com>
2012-01-21 17:56:38 -0500

    fixed dog sorting #35"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,8 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    status = params[:status]
+    link_to title, {:sort => column, :direction => direction, :status => status}, {:class => css_class}
   end
       
 

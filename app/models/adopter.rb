2c2b4613de86025c887d91ad8cfa8f529085e816
Mark Ottaviani <mark.ottaviani@gmail.com>
2012-07-13 19:14:46 -0400

    removed duplicate email vaidation

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -30,12 +30,6 @@ class Adopter < ActiveRecord::Base
   validates :name,  :presence   => true,
     :length     => { :maximum => 50 }
 
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :email, :presence   => true,
-    :format     => { :with => email_regex },
-    :uniqueness => { :case_sensitive => false }
-
   validates :phone, :presence	  => true,
     :length     => {:in => 10..25 }
 

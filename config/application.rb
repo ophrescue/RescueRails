c7d00f9d2b00ba128e737adc7210c3a769d74e37
Mark Ottaviani <mark.ottaviani@gmail.com>
2012-12-01 15:06:34 -0500

    ssl support #143

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ end
 
 module RescueRails
   class Application < Rails::Application
+
+    #Force SSL
+    config.force_ssl = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

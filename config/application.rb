5afaaa7d7b60018a52f832a27624631842c9ba4d
Mark Ottaviani <mark.ottaviani@gmail.com>
2012-12-01 17:39:41 -0500

    sever config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ end
 module RescueRails
   class Application < Rails::Application
 
+    config.force_ssl = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

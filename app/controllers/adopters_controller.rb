2e9db580cc2b738a50ae4d3babd5cb8b2345a205
Mark Ottaviani <mark.ottaviani@gmail.com>
2012-12-09 15:59:15 -0500

    #159 improved return to adoption app button

--- app/controllers/adopters_controller.rb
+++ app/controllers/adopters_controller.rb
@@ -27,6 +27,7 @@ class AdoptersController < ApplicationController
   end
 
   def show
+    session[:return_to] ||= request.referer
     @adopter = Adopter.find(params[:id])
     @adoption_app = @adopter.adoption_app
     @title = @adopter.name

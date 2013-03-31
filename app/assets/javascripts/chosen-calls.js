a240476fc6906a724566106ace5c89ed6909a3e0
Mark Ottaviani <mark.ottaviani@gmail.com>
2013-03-30 23:27:25 -0400

    #222 can unassign fosters and ACs from dogs also search the drop down

--- app/assets/javascripts/chosen-calls.js
+++ app/assets/javascripts/chosen-calls.js
@@ -3,4 +3,6 @@ jQuery(function($){
 	$('#dog_user_id').chosen({allow_single_deselect:true});
 	$('#dog_primary_breed_id').chosen();
 	$('#dog_secondary_breed_id').chosen();
+	$('#dog_foster_id').chosen();
+	$('#dog_coordinator_id').chosen();
 });

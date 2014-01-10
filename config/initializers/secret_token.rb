ee4acb6f79ea72683bea9a8ea10944a234fbf8fb
Mark Ottaviani <mark.ottaviani@gmail.com>
2014-01-09 22:19:44 -0500

    fix silly error

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,7 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyApp::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+RescueRails::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
   ('x' * 30) # meets minimum requirement of 30 chars long
 else
   ENV['SECRET_TOKEN']

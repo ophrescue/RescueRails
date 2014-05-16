5aad29c90f1c0cf9f0089a0fb7d0a5ab07d6c510
Mark Ottaviani <mark.ottaviani@gmail.com>
2014-05-16 14:14:29 -0400

    adding staging configuration for #294

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,54 +1,52 @@
+set :stage, :staging
+set :branch, "staging"
 
-####Todo Create a staging environmemnt for overhauls and major features!
+# used in case we're deploying multiple versions of the same
+# app side by side. Also provides quick sanity checks when looking
+# at filepaths
+set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+#set :server_name, "husky.ophrescue.org"
+set :server_name, "beagle.ophrescue.org"
 
-# set :stage, :production
-# set :branch, "master"
+server 'beagle.ophrescue.org', user: 'deploy', roles: %w{web app db}, primary: true
 
-# # used in case we're deploying multiple versions of the same
-# # app side by side. Also provides quick sanity checks when looking
-# # at filepaths
-# set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
-# set :server_name, "www.example.com example.com"
+set :deploy_to, "/var/www/#{fetch(:full_app_name)}"
 
-# server 'www.example.com', user: 'deploy', roles: %w{web app db}, primary: true
+# dont try and infer something as important as environment from
+# stage name.
+set :rails_env, :staging
 
-# set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
+# number of unicorn workers, this will be reflected in
+# the unicorn.rb and the monit configs
+set :unicorn_worker_count, 3
 
-# # dont try and infer something as important as environment from
-# # stage name.
-# set :rails_env, :production
-
-# # number of unicorn workers, this will be reflected in
-# # the unicorn.rb and the monit configs
-# set :unicorn_worker_count, 5
-
-# # whether we're using ssl or not, used for building nginx
-# # config file
-# set :enable_ssl, false
+# whether we're using ssl or not, used for building nginx
+# config file
+set :enable_ssl, true
 
 
-# # Custom SSH Options
-# # ==================
-# # You may pass any option but keep in mind that net/ssh understands a
-# # limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-# #
-# # Global options
-# # --------------
-# #  set :ssh_options, {
-# #    keys: %w(/home/rlisowski/.ssh/id_rsa),
-# #    forward_agent: false,
-# #    auth_methods: %w(password)
-# #  }
-# #
-# # And/or per server (overrides global)
-# # ------------------------------------
-# # server 'example.com',
-# #   user: 'user_name',
-# #   roles: %w{web app},
-# #   ssh_options: {
-# #     user: 'user_name', # overrides user setting above
-# #     keys: %w(/home/user_name/.ssh/id_rsa),
-# #     forward_agent: false,
-# #     auth_methods: %w(publickey password)
-# #     # password: 'please use keys'
-# #   }
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }

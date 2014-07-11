627d9edd8e475c6ccb44a1b06def391c9b5c1ccb
Shaun Butler <shaun.butler@gmail.com>
2014-07-11 00:36:41 -0400

    Clean up whitespace and hash syntax

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -16,45 +16,43 @@
 #
 
 class Attachment < ActiveRecord::Base
+  attr_accessible :attachable_id,
+          :attachable_type,
+          :attachment_content_type,
+          :attachment_file_name,
+          :attachment_file_size,
+          :attachment_updated_at,
+          :attachment,
+          :updated_by_user_id,
+          :description
 
-	attr_accessible :attachable_id, 
-					:attachable_type, 
-					:attachment_content_type, 
-					:attachment_file_name, 
-					:attachment_file_size, 
-					:attachment_updated_at,
-					:attachment,
-					:updated_by_user_id,
-					:description
+  belongs_to :attachable, polymorphic: true
 
-	belongs_to :attachable, :polymorphic => true
+  belongs_to :updated_by_user, class_name: 'User'
 
-	belongs_to :updated_by_user, :class_name => 'User'
+  has_attached_file :attachment,
+            path: ':rails_root/public/system/attachments/:hash.:extension',
+            url: '/system/attachments/:hash.:extension',
+            hash_secret: 'e17ac013aa7f8f2fd095edfa012edb8c',
+            s3_permissions: :private
 
-	has_attached_file :attachment, 
-					  :path => ":rails_root/public/system/attachments/:hash.:extension",
-					  :url  => "/system/attachments/:hash.:extension",
-					  :hash_secret => "e17ac013aa7f8f2fd095edfa012edb8c",
-					  :s3_permissions => :private
+  validates_attachment_presence :attachment
+  validates_attachment_size :attachment, less_than: 10.megabytes
+  validates_attachment_content_type :attachment,
+   content_type: ['image/jpg','image/jpeg','image/pjpeg','image/png','image/x-png','image/gif','application/pdf',
+            'application/msword','applicationvnd.ms-word','application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            'application/msexcel','application/vnd.ms-excel','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'application/mspowerpoint','application/vnd.ms-powerpoint','application/vnd.openxmlformats-officedocument.presentationml.presentation',
+            'text/plain'],
+   message: 'Images, Docs, PDF, Excel and Plain Text Only.'
 
-
-	validates_attachment_presence :attachment
-	validates_attachment_size :attachment, :less_than => 10.megabytes
-
-	validates_attachment_content_type :attachment,
-	 :content_type => ['image/jpg','image/jpeg','image/pjpeg','image/png','image/x-png','image/gif','application/pdf',
-						'application/msword','applicationvnd.ms-word','application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-						'application/msexcel','application/vnd.ms-excel','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-						'application/mspowerpoint','application/vnd.ms-powerpoint','application/vnd.openxmlformats-officedocument.presentationml.presentation',
-						'text/plain'],
-	 :message => 'Images, Docs, PDF, Excel and Plain Text Only.'
-
-  def download_url(style_name=:original)
+  def download_url(style_name = :original)
     s3 = AWS::S3.new
     @bucket ||= s3.buckets[attachment.bucket_name]
-    @bucket.objects[attachment.s3_object(style_name).key].url_for(:read,
-        :secure => true,
-        :expires => 300,
-        :response_content_disposition => "attachment; filename='#{attachment_file_name}'").to_s
+    @bucket.objects[attachment.s3_object(style_name).key].url_for(
+      :read,
+      secure: true,
+      expires: 300,
+      response_content_disposition: "attachment; filename='#{attachment_file_name}'").to_s
   end
 end

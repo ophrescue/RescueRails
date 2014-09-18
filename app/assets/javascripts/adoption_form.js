27cc9ceeecd9f0fc8c41b8f8622092903dd5fdfa
Shaun Butler <shaun.butler@gmail.com>
2014-09-18 08:01:52 -0400

    Full element swap version
    * Removes using disabled fields

--- app/assets/javascripts/adoption_form.js
+++ app/assets/javascripts/adoption_form.js
@@ -7,9 +7,13 @@ $( function(){
       if (isEditing) {
         // find all disabled fields in .edit-adopter and enable them
         $('form.edit-adopter .to-disable').attr('disabled', 'disabled');
+        $('.editable').hide();
+        $('.read-only').show();
       }
       else {
         $('form.edit-adopter :disabled').removeAttr('disabled').addClass('to-disable');
+        $('.editable').show();
+        $('.read-only').hide();
       }
 
       isEditing = !isEditing;

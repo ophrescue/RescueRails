392bfcbedc3a01ccf928e8314173766e659a0e56
Shaun Butler <shaun.butler@gmail.com>
2014-10-22 17:47:16 -0400

    Make ready_to_adopt_dt editable

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('#adopter_adoption_app_attributes_ready_to_adopt_dt').datepicker({
+  $('#adoption_app_ready_to_adopt_dt').datepicker({
     dateFormat: 'yy-mm-dd',
     maxDate: '+9w',
     minDate: '0d',

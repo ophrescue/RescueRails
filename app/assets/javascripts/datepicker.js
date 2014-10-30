958be90e857bc89d6e47cac5eaf20778a7e9f41d
Mark Ottaviani <mark.ottaviani@gmail.com>
2014-10-29 23:06:19 -0400

    datepicker for ready to adopt is needed in two spots.  When an adopter is creating an application and when a staff member updates the attribute.  We were overwriting each others changes

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -6,6 +6,12 @@ $(function() {
     onSelect: RescueRails.saveParentForm
    });
 
+  $('#adopter_adoption_app_attributes_ready_to_adopt_dt').datepicker({
+    dateFormat: 'yy-mm-dd',
+    maxDate: '+9w',
+    minDate: '0d'
+   });
+
   $('#event_event_date').datepicker({
     dateFormat: 'yy-mm-dd',
     maxDate: '+6m',

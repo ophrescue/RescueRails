39dc0001fc831d3e4e51e2c5f065816057f9f5ff
Shaun Butler <shaun.butler@gmail.com>
2015-02-19 08:59:21 -0500

    Pull common SQL out to methods

--- app/models/dog_searcher.rb
+++ app/models/dog_searcher.rb
@@ -1,4 +1,6 @@
 class DogSearcher
+  PER_PAGE = 30
+
   def initialize(params: {}, manager: false)
     @params = params
     @manager = manager
@@ -7,26 +9,43 @@ class DogSearcher
   def search
     if @manager
       if (@params[:search].to_i > 0)
-        dogs = Dog.where('tracking_id = ?', "#{@params[:search].to_i}").paginate(:page => @params[:page])
+        @dogs = Dog.where('tracking_id = ?', "#{@params[:search].to_i}")
       elsif @params[:search]
-        dogs = Dog.where('lower(name) LIKE ?', "%#{@params[:search].downcase.strip}%").paginate(:page => @params[:page])
+        @dogs = Dog.where('lower(name) LIKE ?', "%#{@params[:search].downcase.strip}%")
       elsif @params[:status] == 'active'
         statuses = ['adoptable', 'adoption pending', 'on hold', 'return pending', 'coming soon']
-        dogs = Dog.where("status IN (?)", statuses).order(sort_column + ' ' + sort_direction).paginate(:per_page => 30, :page => @params[:page]).includes(:photos, :primary_breed)
+        @dogs = Dog.where("status IN (?)", statuses)
       elsif @params.has_key? :status
-        dogs = Dog.where(:status => @params[:status]).order(sort_column + ' ' + sort_direction).paginate(:per_page => 30, :page => @params[:page]).includes(:photos, :primary_breed)
+        @dogs = Dog.where(:status => @params[:status])
       else
-        dogs = Dog.where("name ilike ?", "%#{@params[:q]}%").order(sort_column + ' ' + sort_direction).paginate(:per_page => 30, :page => @params[:page]).includes(:photos, :primary_breed)
+        @dogs = Dog.where("name ilike ?", "%#{@params[:q]}%")
       end
     else
       statuses = ['adoptable', 'adoption pending', 'coming soon']
-      dogs = Dog.where("status IN (?)", statuses).order(sort_column + ' ' + sort_direction).paginate(:per_page => 30, :page => @params[:page]).includes(:photos, :primary_breed)
+      @dogs = Dog.where("status IN (?)", statuses)
     end
-    dogs
+
+    includes
+    sort
+    paginate
+
+    @dogs
   end
 
   def self.search(params: {}, manager: false)
     self.new(params: params, manager: manager).search
+  end
+
+  def includes
+    @dogs = @dogs.includes(:photos, :primary_breed)
+  end
+
+  def sort
+    @dogs = @dogs.order(sort_column + ' ' + sort_direction)
+  end
+
+  def paginate
+    @dogs = @dogs.paginate(:per_page => PER_PAGE, :page => @params[:page])
   end
 
   def sort_column

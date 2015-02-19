b398439ae81f6387386f50265f57a8f0f75c3ac3
Shaun Butler <shaun.butler@gmail.com>
2015-02-19 09:07:27 -0500

    Add readability for conditions

--- app/models/dog_searcher.rb
+++ app/models/dog_searcher.rb
@@ -1,5 +1,7 @@
 class DogSearcher
   PER_PAGE = 30
+  ACTIVE_STATUSES = ['adoptable', 'adoption pending', 'on hold', 'return pending', 'coming soon']
+  PUBLIC_STATUSES = ['adoptable', 'adoption pending', 'coming soon']
 
   def initialize(params: {}, manager: false)
     @params = params
@@ -8,21 +10,19 @@ class DogSearcher
 
   def search
     if @manager
-      if (@params[:search].to_i > 0)
+      if tracking_id_search?
         @dogs = Dog.where('tracking_id = ?', "#{@params[:search].to_i}")
-      elsif @params[:search]
+      elsif name_search?
         @dogs = Dog.where('lower(name) LIKE ?', "%#{@params[:search].downcase.strip}%")
-      elsif @params[:status] == 'active'
-        statuses = ['adoptable', 'adoption pending', 'on hold', 'return pending', 'coming soon']
-        @dogs = Dog.where("status IN (?)", statuses)
-      elsif @params.has_key? :status
+      elsif active_status_search?
+        @dogs = Dog.where("status IN (?)", ACTIVE_STATUSES)
+      elsif status_search?
         @dogs = Dog.where(:status => @params[:status])
       else
         @dogs = Dog.where("name ilike ?", "%#{@params[:q]}%")
       end
     else
-      statuses = ['adoptable', 'adoption pending', 'coming soon']
-      @dogs = Dog.where("status IN (?)", statuses)
+      @dogs = Dog.where("status IN (?)", PUBLIC_STATUSES)
     end
 
     includes
@@ -34,6 +34,24 @@ class DogSearcher
 
   def self.search(params: {}, manager: false)
     self.new(params: params, manager: manager).search
+  end
+
+  private
+
+  def tracking_id_search?
+    @params[:search].to_i > 0
+  end
+
+  def name_search?
+    @params[:search].present?
+  end
+
+  def active_status_search?
+    @params[:status] == 'active'
+  end
+
+  def status_search?
+    @params.has_key? :status
   end
 
   def includes

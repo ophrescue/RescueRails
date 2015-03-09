8e504cc5a638b835a5eaae0bfa20333e848d022c
Mark Ottaviani <mark.ottaviani@gmail.com>
2015-03-08 21:38:42 -0400

    user controller cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   before_filter :authenticate
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => [:new, :create, :destroy]
@@ -28,13 +28,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
-  
+
   def new
     @user = User.new
     @title = "Add a Staff Account"
     init_fields
   end
-  
+
   def create
     @user = User.new
     @user.accessible = :all if current_user.admin?
@@ -90,8 +90,5 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
-
-
-
 
 end

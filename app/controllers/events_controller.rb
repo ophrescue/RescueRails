591c0963fe3cdbcff8c784498f7962b75fe75370
Mark Ottaviani <mark.ottaviani@gmail.com>
2015-09-22 21:18:43 -0400

    events controller cleanup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,73 +1,73 @@
 class EventsController < ApplicationController
 
 
-	before_filter :edit_events_user, 	:except => [:index, :show]
+  before_filter :edit_events_user,  :except => [:index, :show]
 
-	def index
-		@title = "Upcoming Events"
-		@events = Event.find(:all,
-							 :conditions => ["event_date >= ?", Date.today],
-							 :limit => 30, 
-							 :order => 'event_date')
-	end
+  def index
+    @title = "Upcoming Events"
+    @events = Event.find(:all,
+               :conditions => ["event_date >= ?", Date.today],
+               :limit => 30, 
+               :order => 'event_date')
+  end
 
-	def past
-		@title = "Past Events"
-		@events = Event.find(:all,
-							 :conditions => ["event_date <= ?", Date.today],
-							 :limit => 30, 
-							 :order => 'event_date')
-	end
+  def past
+    @title = "Past Events"
+    @events = Event.find(:all,
+               :conditions => ["event_date <= ?", Date.today],
+               :limit => 30, 
+               :order => 'event_date')
+  end
 
-	def show
-		@title = "Events"
-		@event = Event.find(params[:id])
-	end
+  def show
+    @title = "Events"
+    @event = Event.find(params[:id])
+  end
 
-	def new
-		@title = "Add an Event"
-		@event = Event.new
-	end
+  def new
+    @title = "Add an Event"
+    @event = Event.new
+  end
 
-	def edit
-		@title = "Edit Event"
-		@event = Event.find(params[:id])
-	end
+  def edit
+    @title = "Edit Event"
+    @event = Event.find(params[:id])
+  end
 
-	def update
-		@event = Event.find(params[:id])
-		@event.photo_delete = params[:event][:photo_delete]
-	    if @event.update_attributes(params[:event])
-	      flash[:success] = "Event updated."
-	      redirect_to events_path
-	    else
-	      @title = "Edit Event"
-	      render 'edit'
-	    end
-	end
+  def update
+    @event = Event.find(params[:id])
+    @event.photo_delete = params[:event][:photo_delete]
+      if @event.update_attributes(params[:event])
+        flash[:success] = "Event updated."
+        redirect_to events_path
+      else
+        @title = "Edit Event"
+        render 'edit'
+      end
+  end
 
-	def create
-		@event = Event.new(params[:event])
-		if @event.save
-			flash[:success] = "New Event Added"
-			redirect_to events_path
-		else
-			@title = "Add an Event"
-			render 'new'
-		end
-	end
+  def create
+    @event = Event.new(params[:event])
+    if @event.save
+      flash[:success] = "New Event Added"
+      redirect_to events_path
+    else
+      @title = "Add an Event"
+      render 'new'
+    end
+  end
 
-	def destroy
-		@title = "Events"
-		Event.find(params[:id]).destroy
-		flash[:danger] = "Event Deleted"
-		redirect_to events_path
-	end
+  def destroy
+    @title = "Events"
+    Event.find(params[:id]).destroy
+    flash[:danger] = "Event Deleted"
+    redirect_to events_path
+  end
 
-	private
+  private
 
-	    def edit_events_user
-	      redirect_to(root_path) unless current_user.edit_events?
-	    end
+      def edit_events_user
+        redirect_to(root_path) unless current_user.edit_events?
+      end
 
 end

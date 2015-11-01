9e8dc856349469a983c84978ad2976e63d442fe3
Mark Ottaviani <mark.ottaviani@gmail.com>
2015-11-01 10:56:12 -0500

    improved test coverage

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe DogsController, type: :controller do
+
+  let!(:admin) {create(:user, :admin)}
+
   describe 'GET #index' do
 
     context 'user is logged in' do
@@ -24,7 +27,6 @@ describe DogsController, type: :controller do
   end
 
   describe 'POST create' do
-    let!(:admin) {create(:user, :admin)}
 
     context 'logged in as dog adder admin' do
 
@@ -39,4 +41,20 @@ describe DogsController, type: :controller do
       end
     end
   end
+
+  describe 'PUT update' do
+    let(:test_dog) {create(:dog, name: 'Old Dog Name') }
+    let(:request) { -> {put :update, id: test_dog.id, dog: attributes_for(:dog, name: 'New Dog Name')}}
+
+    context 'logged in as admin' do
+      before :each do
+        allow(controller).to receive(:current_user) {admin}
+      end
+
+      it 'updates the dog name' do
+        expect { request.call }.to change{ test_dog.reload.name }.from('Old Dog Name').to('New Dog Name')
+      end
+    end
+  end
+
 end

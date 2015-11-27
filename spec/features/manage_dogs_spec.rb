b065a04c90b7884dcb32931953535fa83f05f8f6
Mark Ottaviani <mark.ottaviani@gmail.com>
2015-11-27 17:37:50 -0500

    check for titleized dog names in test

--- spec/features/manage_dogs_spec.rb
+++ spec/features/manage_dogs_spec.rb
@@ -5,9 +5,9 @@ feature 'Manage Dogs', js: true do
   scenario 'View an existing dog' do
     test_dog = create(:dog)
     visit '/dogs'
-    expect(page).to have_content(test_dog.name)
+    expect(page).to have_content(test_dog.name.titleize)
     click_link(test_dog.name)
-    expect(page).to have_content(test_dog.name)
+    expect(page).to have_content(test_dog.name.titleize)
   end
 
 end

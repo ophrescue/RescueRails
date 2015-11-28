b91eb23a3f60ed8f954c60c317d61efd3d85f677
Mark Ottaviani <mark.ottaviani@gmail.com>
2015-11-28 11:44:42 -0500

    fixed event ActiveRecord Calls

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,18 +5,16 @@ class EventsController < ApplicationController
 
   def index
     @title = "Upcoming Events"
-    @events = Event.find(:all,
-               conditions: ["event_date >= ?", Date.today],
-               limit: 30,
-               order: 'event_date')
+    @events = Event.where("event_date >= ?", Date.today)
+                   .limit(30)
+                   .order('event_date DESC')
   end
 
   def past
     @title = "Past Events"
-    @events = Event.find(:all,
-               conditions: ["event_date <= ?", Date.today],
-               limit: 30,
-               order: 'event_date')
+    @events = Event.where("event_date < ?", Date.today)
+                   .limit(30)
+                   .order('event_date DESC')
   end
 
   def show

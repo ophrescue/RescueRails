aac3195e8c59f57bbf0073d92c8fb1ed4534a69c
Mark Ottaviani <mark.ottaviani@gmail.com>
2016-01-25 14:55:00 -0500

    add additional tests for existing folder functions

---
+++ spec/controllers/folders_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe FoldersController, type: :controller do
+
+  let!(:admin) {create(:user, :admin)}
+  let!(:user) {create(:user, dl_resources: FALSE)}
+
+  describe 'GET #show' do
+
+    context 'logged in with standard folder access' do
+      before :each do
+        allow(controller).to receive(:current_user) { admin }
+      end
+
+      it 'is able to view files in standard folders' do
+        folder = create(:folder)
+        get :show, id: folder
+        expect(assigns(:folder)).to eq folder
+      end
+      it 'is NOT able to view files in restricted folders'
+    end
+
+
+    context 'logged in without any folder access' do
+      before :each do
+        allow(controller).to receive(:current_user) { user }
+      end
+      it 'is NOT able to view files in standard folders' do
+        folder = create(:folder)
+        get :show, id: folder
+        expect(assigns(:folders)).to match(nil)
+      end
+
+      it 'is NOT able to view files in restricted folders'
+    end
+
+
+    context 'logged in with restricted folder access' do
+      it 'is able to view files in standard folders'
+      it 'is able to view files in restricted folders'
+    end
+  end
+
+end
---
+++ spec/factories/folder.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :folder do
+    name 'Folder Name'
+    description 'Folder Descritpion'
+  end
+end

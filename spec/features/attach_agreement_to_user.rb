a6be9b896c19c6c590ef4ad043de59e728d7af30
Mark Ottaviani <mark.ottaviani@gmail.com>
2016-03-26 15:22:42 -0400

    added stub for paperclip download_url method

--- spec/features/attach_agreement_to_user.rb
+++ spec/features/attach_agreement_to_user.rb
@@ -4,6 +4,10 @@ feature 'Attach an agreement to a user', js: true do
   let!(:admin) {create(:user, :admin)}
   let!(:user) {create(:user)}
 
+  before :each do
+    allow_any_instance_of(Attachment).to receive(:download_url).and_return('http://')
+  end
+
   scenario 'Admin attaches a foster agreement to a user' do
     sign_in(admin)
 
@@ -17,6 +21,7 @@ feature 'Attach an agreement to a user', js: true do
     page.attach_file('user_agreement_attributes_attachment','public/docs/blue-ridge-bloodbank.pdf')
     click_button('Update / Verify')
     expect(page).to have_content(user.name)
+    expect(page).to have_no_content('No Agreement on File')
     expect(page).to have_content('blue-ridge-bloodbank.pdf')
 
   end

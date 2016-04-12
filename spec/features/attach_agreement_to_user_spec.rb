98365c2febc66e178d4ee172227bdd6fbc7e94bd
Mark Ottaviani <mark.ottaviani@gmail.com>
2016-04-11 20:35:21 -0400

    test checks if agreements are overwriting each other

--- spec/features/attach_agreement_to_user_spec.rb
+++ spec/features/attach_agreement_to_user_spec.rb
@@ -18,10 +18,14 @@ feature 'Attach an agreement to a user', js: true do
     expect(page).to have_content('No Foster Agreement on File')
     click_link('Update/Verify Profile')
     expect(page).to have_content('Edit Staff Account')
+
     page.attach_file('user_agreement_attributes_attachment','public/docs/blue-ridge-bloodbank.pdf')
     click_button('Update / Verify')
-    expect(page).to have_content(user.name)
-    expect(page).to have_no_content('No Foster Agreement on File')
+
+    expect(page).to have_no_css('#confidentiality-agreement-dl')
+    within('#foster-agreement-dl') do
+      expect(page).to have_content 'blue-ridge-bloodbank'
+    end
   end
 
   scenario 'Admin attaches a confidentiality agreement to a user' do
@@ -34,10 +38,14 @@ feature 'Attach an agreement to a user', js: true do
     expect(page).to have_content('No Confidentiality Agreement on File')
     click_link('Update/Verify Profile')
     expect(page).to have_content('Edit Staff Account')
-    page.attach_file('user_confidentiality_agreement_attributes_attachment','public/docs/blue-ridge-bloodbank.pdf')
+
+    page.attach_file('user_confidentiality_agreement_attributes_attachment','public/docs/guide-to-adopting.pdf')
     click_button('Update / Verify')
-    expect(page).to have_content(user.name)
-    expect(page).to have_no_content('No Confidentiality Agreement on File')
-    expect(page).to have_content('Confidentiality Agreement on File')
+
+    expect(page).to have_no_css('#foster-agreement-dl')
+    within('#confidentiality-agreement-dl') do
+      expect(page).to have_content 'guide-to-adopting'
+    end
+
   end
 end

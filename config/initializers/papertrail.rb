564b92c50602a9949132b8be8dde3ef2a9362650
Shaun Butler <shaun.butler@gmail.com>
2016-07-12 22:19:19 -0400

    Reverts changes for now

--- config/initializers/papertrail.rb
+++
@@ -1,9 +0,0 @@
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:s3_permissions] = 'private'
-Paperclip::Attachment.default_options[:s3_credentials] = {
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  bucket: ENV['S3_BUCKET_NAME'],
-  s3_region: ENV['AWS_REGION']
-}
-Paperclip::Attachment.default_options[:url] = ":s3_domain_url"

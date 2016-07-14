9e76a128cc02e4c1a177ac5d41fa47d93163ba2a
Shaun Butler <shaun.butler@gmail.com>
2016-07-13 21:08:31 -0400

    Upgrade gem aws sdk (#455)
    * Updates paperclip and awk-sdk gems
    
    still WIP
    
    * Use ENV vars for AWS credentials
    
    * Reverts changes for now
    
    * Redefines download_url for attachments
    
    The AWS SDK v2 way.
    
    * added s3 region settings for other envs

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -25,11 +25,10 @@ class Photo < ActiveRecord::Base
                    medium: '320x320',
                    thumb: 'x195',
                    minithumb: 'x64#' },
-            s3_permissions: :public_read,
+            s3_permissions: "public-read",
             path: ":rails_root/public/system/dog_photo/:hash.:extension",
             url: "/system/dog_photo/:hash.:extension",
             hash_secret: "80fd0acd1674d7efdda5b913a7110d5c955e2d73"
-
 
   validates_attachment_presence :photo
   validates_attachment_size :photo, less_than: 10.megabytes

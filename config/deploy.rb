3d0c9d72dd1a00f48965d1c12b8c6cd2fda0e1e1
Mark Ottaviani <mark.ottaviani@gmail.com>
2016-08-01 19:47:30 -0400

    update ruby version in cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :default_environment, { 'PATH' => '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH
 
 # setup rbenv
 set :rbenv_type, :user
-set :rbenv_ruby, '2.2.3'
+set :rbenv_ruby, '2.3.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 

92d8c75411356b9bef6d755ede77c15876cbaea9
Shaun Butler <shaun.butler@gmail.com>
2016-08-05 11:30:34 -0400

    Adds EventsController tests

---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+describe EventsController, type: :controller do
+  describe 'GET #index' do
+    it 'is successful' do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #past' do
+    include_context 'signed in admin'
+
+    it 'is successful' do
+      get :past
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #show' do
+    let(:event) { create(:event) }
+
+    it 'is successful' do
+      get :show, id: event.id
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #new' do
+    include_context 'signed in admin'
+
+    it 'is successful' do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #edit' do
+    include_context 'signed in admin'
+
+    let(:event) { create(:event) }
+
+    it 'is successful' do
+      get :edit, id: event.id
+      expect(response).to be_success
+    end
+  end
+
+  describe 'PUT #update' do
+    include_context 'signed in admin'
+
+    let(:event) { create(:event) }
+
+    it 'is successful' do
+      put :update, id: event.id, event: { title: 'Event' }
+      expect(flash[:success]).to be
+      expect(response).to redirect_to events_path
+    end
+  end
+
+  describe 'POST #create' do
+    include_context 'signed in admin'
+
+    it 'is successful' do
+      post :create, event: attributes_for(:event)
+      expect(flash[:success]).to be
+      expect(response).to redirect_to events_path
+    end
+  end
+
+  describe 'POST #delete' do
+    include_context 'signed in admin'
+
+    let(:event) { create(:event) }
+
+    it 'is successful' do
+      delete :destroy, id: event.id
+      expect(flash[:danger]).to be
+      expect(response).to redirect_to events_path
+    end
+  end
+end

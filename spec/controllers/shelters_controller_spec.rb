47ecf2890d303ed11201de75b3ec6c2b86c9ae92
Shaun Butler <shaun.butler@gmail.com>
2016-08-09 09:41:15 -0400

    Adds SheltersController tests

--- spec/controllers/shelters_controller_spec.rb
+++ spec/controllers/shelters_controller_spec.rb
@@ -11,15 +11,51 @@
 require 'rails_helper'
 
 describe SheltersController, type: :controller do
-
-  let!(:admin) { create(:user, :admin) }
   let!(:hacker) { create(:user) }
 
-  describe 'POST create' do
+  describe 'GET #index' do
+    include_context 'signed in admin'
+
+    it 'is successful' do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #show' do
+    include_context 'signed in admin'
+
+    let(:shelter) { create(:shelter) }
+
+    it 'is successful' do
+      get :show, id: shelter.id
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #new' do
+    include_context 'signed in admin'
+
+    it 'is successful' do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #edit' do
+    include_context 'signed in admin'
+
+    let(:shelter) { create(:shelter) }
+
+    it 'is successful' do
+      get :edit, id: shelter.id
+      expect(response).to be_success
+    end
+  end
+
+  describe 'POST #create' do
     context 'logged in as an admin' do
-      before :each do
-        allow(controller).to receive(:current_user) { admin }
-      end
+      include_context 'signed in admin'
 
       it 'is able to create a shelter' do
         expect{
@@ -36,26 +72,21 @@ describe SheltersController, type: :controller do
       it 'is unable to create a shelter' do
         expect{
           post :create, shelter: attributes_for(:shelter)
-        }.to change(Shelter, :count).by(0)
+        }.to_not change(Shelter, :count)
       end
-
     end
-
   end
 
-  describe 'PUT update' do
+  describe 'PUT #update' do
     let(:test_shelter) { create(:shelter, name: 'BARCS') }
     let(:request) { -> { put :update, id: test_shelter.id, shelter: attributes_for(:shelter, name: 'New Shelter') } }
 
     context 'logged in as admin' do
-      before :each do
-        allow(controller).to receive(:current_user) { admin }
-      end
+      include_context 'signed in admin'
 
       it 'updates the shelter name' do
         expect { request.call }.to change { test_shelter.reload.name }.from('BARCS').to('New Shelter')
       end
-
     end
 
     context 'logged in as normal user' do
@@ -66,9 +97,6 @@ describe SheltersController, type: :controller do
       it 'is unable to modify shelter' do
         expect { request.call }.to_not change { test_shelter.reload.name }
       end
-
     end
-
   end
-
 end

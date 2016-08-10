f54fc9811d0f48fc3b2aac2fc81be10d4b43c51e
Shaun Butler <shaun.butler@gmail.com>
2016-08-10 08:55:35 -0400

    Adds tests for PhotosController

---
+++ spec/controllers/photos_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe PhotosController, type: :controller do
+  include_context 'signed in admin'
+
+  let(:dog) { create(:dog_with_photo_and_attachment) }
+  let(:photo) { create(:photo, dog: dog) }
+
+  describe 'POST #sort' do
+    it 'is successful' do
+      post :sort, dog_id: dog.id, photo: [photo]
+      expect(response).to be_successful
+    end
+  end
+end

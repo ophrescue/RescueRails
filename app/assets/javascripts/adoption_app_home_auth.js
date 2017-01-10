1a857da11aacec3c5e14863fc9accec123eb1467
Mark Ottaviani <mark.ottaviani@gmail.com>
2017-01-09 22:32:44 -0500

    format cleanup

--- app/assets/javascripts/adoption_app_home_auth.js
+++ app/assets/javascripts/adoption_app_home_auth.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    $( ".own-home-auth" ).hide();
-    $(".residence-auth-pop").change(function() {
-        if (this.value == 'own'){
-            $( ".own-home-auth" ).show();
-    }
-    else if (this.value == 'rent') {
-        $( ".own-home-auth" ).hide();
-    }
+$(document).ready(function () {
+    $(".own-home-auth").hide();
+    $(".residence-auth-pop").change(function () {
+        if (this.value == 'own') {
+            $(".own-home-auth").show();
+        }
+        else if (this.value == 'rent') {
+            $(".own-home-auth").hide();
+        }
     });
 });

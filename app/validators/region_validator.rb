a9c041a563ec36543c1517387a5f223d1622aedd
Toby Murray <murray.toby@gmail.com>
2017-10-15 13:45:25 -0400

    #437: Add region validator
    Adds support for checking the province is valid when country is Canada.

---
+++ app/validators/region_validator.rb
@@ -0,0 +1,43 @@
+class RegionValidator < ActiveModel::Validator
+  PROVINCES = %w[AB BC MB NB NL NS ON PE QC SK NT NU YT].freeze
+
+  def validate(record)
+    country = ISO3166::Country.find_country_by_alpha3(record.country)
+
+    unless country
+      record.errors[:region] << "cannot be validated without a value for country."
+      return
+    end
+
+    if country.eql? ISO3166::Country[:us]
+      validate_state(record)
+    elsif country.eql? ISO3166::Country[:ca]
+      validate_province(record)
+    end
+  end
+
+  private
+
+  def validate_state(record)
+    if record.region.blank?
+      record.errors[:region] << "State cannot be blank."
+      return
+    end
+
+    record.errors[:region] << "State is the wrong length (should be 2 letters)." unless record.region.length == 2
+  end
+
+  def validate_province(record)
+    if record.region.blank?
+      record.errors[:region] << "Province cannot be blank."
+      return
+    end
+
+    unless record.region.length == 2
+      record.errors[:region] << "Province is the wrong length (should be 2 letters)."
+      return
+    end
+
+    record.errors[:region] << "'#{record.region}' is not a valid province." unless PROVINCES.include? record.region
+  end
+end

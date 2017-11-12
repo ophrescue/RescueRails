d5f590baa5765218d1a2a1e0134e13e4e2803fda
Tara <tjaikaeo@gmail.com>
2017-11-11 23:24:19 -0500

    Added edit reference feature test

---
+++ spec/features/edit_reference_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'Edit Reference Form', js: true do
+  let!(:admin) { create(:user, :admin) }
+  let!(:adopter) { create(:adopter_with_app) }
+
+  scenario 'Admin edits reference' do
+    reference = FactoryBot.create(:reference, adopter: adopter)
+
+    sign_in(admin)
+    visit adopter_path(adopter)
+
+    expect(page).to have_content(adopter.name)
+    expect(page).to have_content(adopter.email)
+
+    click_link('References')
+    expect(page).to have_field("adopter_references_attributes_0_name", with: reference.name)
+    expect(page).to have_field("adopter_references_attributes_0_email", with: reference.email)
+    expect(page).to have_field("adopter_references_attributes_0_whentocall", with: reference.whentocall)
+
+    fill_in('adopter_references_attributes_0_name', with: 'First Reference')
+    within(:css, "#references") do
+      click_button('Save')
+    end
+
+    expect(page).to have_content('Application Updated')
+    
+    click_link('References')
+    expect(page).to have_field("adopter_references_attributes_0_name", with: 'First Reference')
+  end
+end

22916624b9401066054a8db8b2c2a74bbcda959d
Mark Ottaviani <mark.ottaviani@gmail.com>
2017-11-23 21:03:27 -0500

    removed duplicate driver and rubocop fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,22 +12,14 @@ require 'capybara-screenshot/rspec'
 # Headless Chrome
 require 'selenium/webdriver'
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :selenium do |app|
+Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu window-size=1366,768) }
+    chromeOptions: { args: %w[headless disable-gpu window-size=1366,768] }
   )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+  Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
 end
 
-Capybara.javascript_driver = :selenium
-
+Capybara.javascript_driver = :headless_chrome
 
 Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |file| require file }
 

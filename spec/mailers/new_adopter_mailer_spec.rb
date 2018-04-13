575c95c8c7ae8b7deeb23c0c91e436125cda3e9d
Mark Ottaviani <mark.ottaviani@gmail.com>
2018-04-13 19:26:27 -0400

    new mailer method tests

---
+++ spec/mailers/new_adopter_mailer_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe NewAdopterMailer, type: :mailer do
+  include ActiveJob::TestHelper
+
+  describe "Application Received! Email" do
+    let(:adopter) { create(:adopter_with_app) }
+
+    it 'job is created' do
+      ActiveJob::Base.queue_adapter = :test
+      expect do
+        NewAdopterMailer.adopter_created(adopter.id).deliver_later
+      end.to have_enqueued_job.on_queue('mailers')
+    end
+
+    it 'welcome_email is sent' do
+      expect do
+        perform_enqueued_jobs do
+          NewAdopterMailer.adopter_created(adopter.id).deliver_later
+        end
+      end.to change { ActionMailer::Base.deliveries.size }.by(1)
+    end
+
+    it 'welcome_email is sent to the right user' do
+      perform_enqueued_jobs do
+        NewAdopterMailer.adopter_created(adopter.id).deliver_later
+      end
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.to[0]).to eq adopter.email
+    end
+  end
+
+  describe "PLEASE REPLY Application Email" do
+    let(:adopter) { create(:adopter_with_app) }
+
+    it 'job is created' do
+      ActiveJob::Base.queue_adapter = :test
+      expect do
+        AdoptAppMailer.adopt_app(adopter.id).deliver_later
+      end.to have_enqueued_job.on_queue('mailers')
+    end
+
+    it 'welcome_email is sent' do
+      expect do
+        perform_enqueued_jobs do
+          AdoptAppMailer.adopt_app(adopter.id).deliver_later
+        end
+      end.to change { ActionMailer::Base.deliveries.size }.by(1)
+    end
+
+    it 'welcome_email is sent to the right user' do
+      perform_enqueued_jobs do
+        AdoptAppMailer.adopt_app(adopter.id).deliver_later
+      end
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.reply_to[0]).to eq adopter.email
+    end
+  end
+end

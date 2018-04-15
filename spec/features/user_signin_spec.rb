4e53a5de4ab15eabbf1a706c785eae5cf83953e0
Les Nightingill <codehacker@comcast.net>
2018-04-15 14:27:15 -0700

    fix for issue # 707

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 feature 'Sign in as Admin', js: true do
-  scenario 'Valid User' do
-    admin = create(:user)
+  let(:admin) { create(:user, :add_dogs => true) }
 
+  scenario 'Valid User' do
     visit '/signin'
-    fill_in('session_email', with: admin.email)
-    fill_in('session_password', with: admin.password)
-    click_button('Sign in')
+    fill_and_submit(admin)
     expect(page).to have_no_content('Invalid')
+  end
+
+  # repro for issue #707
+  scenario 'Redirect to Original Destination' do
+    visit '/dogs/new'
+    expect(page).to have_content('Please sign in to access this page')
+    fill_and_submit(admin)
+    expect(current_path).to eql('/dogs/new')
+  end
+
+  # test for side-effects of issue #707 fix
+  scenario 'Original Destination Redirect is Cleared' do
+    visit '/dogs/new'
+    visit '/signin'
+    fill_and_submit(admin)
+    expect(current_path).to eql('/')
   end
 end
--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,13 @@
 module LoginMacros
-  def sign_in(user)
-    visit '/signin'
+  def fill_and_submit(user)
     fill_in('session_email', with: user.email)
     fill_in('session_password', with: user.password)
     click_button('Sign in')
+  end
+
+  def sign_in(user)
+    visit '/signin'
+    fill_and_submit(user)
     expect(page).to have_content('Staff')
     expect(page).to have_no_content('Invalid')
   end

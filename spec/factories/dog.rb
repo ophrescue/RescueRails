fd97b73a376bfbc88cec714c41b15ea9750ffd9c
Les Nightingill <codehacker@comcast.net>
2018-04-16 16:06:46 -0700

    augment seed data for dogs, populate comments and breeds

--- spec/factories/dog.rb
+++ spec/factories/dog.rb
@@ -1,9 +1,30 @@
 FactoryBot.define do
   factory :dog do
     sequence(:tracking_id) { |n| n }
-    name { Faker::Name.name }
-    status 'adoptable'
+    name { 
+      until(nn = Faker::Dog.name; !Dog.pluck(:name).include?(nn))
+      end
+      nn
+    }
+    status {  Dog::STATUSES.sample }
     sequence(:microchip) { |n| "MC-#{n}" }
+    age { Dog::AGES.sample }
+    size { Dog::SIZES.sample }
+    gender { Dog::GENDERS.sample }
+    medical_review_complete { [true,false].sample }
+    has_medical_need { [true, false].sample }
+    is_high_priority { [true, false].sample }
+    needs_photos { [true, false].sample }
+    has_behavior_problem { [true, false].sample }
+    needs_foster { [true, false].sample }
+    is_altered { [true, false].sample }
+    intake_dt { Date.today.advance(:days => -rand(365)) }
+    primary_breed_id { Breed.pluck(:id).sample }
+    secondary_breed_id { Breed.pluck(:id).sample }
+
+    after(:create) do |dog|
+      create(:comment, :commentable_type => 'Dog', :commentable_id => dog.id, :content => Faker::Lorem.sentence )
+    end
 
     factory :dog_with_photo_and_attachment do
       after(:build) do |dog|

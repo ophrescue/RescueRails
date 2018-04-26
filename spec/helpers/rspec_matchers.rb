dc735d0a6d7874d0e7629c2dcbeb07100f3a3736
Les Nightingill <codehacker@comcast.net>
2018-04-26 09:53:18 -0700

    fix edge case for custom rspec matcher

--- spec/helpers/rspec_matchers.rb
+++ spec/helpers/rspec_matchers.rb
@@ -12,6 +12,11 @@ RSpec::Matchers.define :be_sorted do |expected|
              else raise "unknown sort direction parameter in be_sorted rspec expectation"
              end
 
+      pair = pair.map do |thing|
+        # b/c ruby does not compare strings in the same way as sql
+        thing.is_a?(String) ? thing.gsub(/\s/,'').downcase : thing
+      end
+
       matches << (pair[1] >= pair[0])
     end
     matches.all?

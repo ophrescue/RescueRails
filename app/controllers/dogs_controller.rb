021cab0bb758fd2e1ace06e22c22ea63d585ad3f
Les Nightingill <codehacker@comcast.net>
2018-05-25 08:11:00 -0700

    refactor 'dog unavailable' message out of dogs/show.erb view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -112,6 +112,7 @@ class DogsController < ApplicationController
 
   def show
     @title = @dog.name
+    flash[:error]= render_to_string partial: 'unavailable_flash_message' if @dog.unavailable?
   end
 
   def new

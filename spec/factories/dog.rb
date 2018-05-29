c8d7bc3c1dc7be524335c68ff1444c4c09ff96d2
Les Nightingill <codehacker@comcast.net>
2018-05-29 12:50:34 -0700

    add more fields to dog factory

--- spec/factories/dog.rb
+++ spec/factories/dog.rb
@@ -23,6 +23,12 @@ FactoryBot.define do
     intake_dt { Date.today.advance(:days => -rand(365)) }
     primary_breed_id { Breed.pluck(:id).sample }
     secondary_breed_id { Breed.pluck(:id).sample }
+    is_uptodateonshots { [true, false].sample }
+    is_special_needs        { [true, false].sample }
+    no_dogs                 { [true, false].sample }
+    no_cats                 { [true, false].sample }
+    no_kids                 { [true, false].sample }
+    description { Faker::Lorem.paragraphs(3,true).join("\n\n") }
 
     after(:create) do |dog|
       create(:comment, :commentable_type => 'Dog', :commentable_id => dog.id, :content => Faker::Lorem.sentence )

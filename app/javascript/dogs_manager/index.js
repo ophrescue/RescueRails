c2e1959ae20299518b11268e04c822da8521496a
Les Nightingill <codehacker@comcast.net>
2018-06-11 11:26:21 -0700

    reorganize files and directories for dogs_manager webpacker files

--- app/javascript/dogs_manager/index.js
+++
@@ -1,1 +0,0 @@
-export default { woof: "bite me" }
--- spec/javascript/first_js.spec.js
+++ spec/javascript/first_js.spec.js
@@ -1,10 +1,10 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import App from '../../app/javascript/app.vue'
+import App from '../../app/javascript/packs/components/app.vue'
 
 describe('app.vue', () => {
-  it('shows the Hello Vue message', () => {
+  it('shows the dogs message', () => {
     const wrapper = shallowMount(App)
-    expect(wrapper.find('p').text()).contains('Hello Vue!')
+    expect(wrapper.find('p').text()).contains('who let the dogs out')
   })
 })

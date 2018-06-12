657d3416ebfdf473c63d7d986d662aa8ca610d37
Shaun Butler <shaun.butler@gmail.com>
2018-06-12 09:05:14 -0400

    Removes custom validations to only use clearance ones

--- app/models/user.rb
+++ app/models/user.rb
@@ -89,20 +89,8 @@ class User < ApplicationRecord
 
   strip_attributes only: :email
 
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :name,  presence: true,
                     length: { maximum: 50 }
-
-  validates :email, presence: true,
-                    format: { with: email_regex },
-                    uniqueness: { case_sensitive: false }
-
-  # Automatically creates the virtual attribute 'password_confirmation'.
-  validates :password, presence: true,
-                       confirmation: true,
-                       length: { within: 8..40 },
-                       unless: :skip_password_validation?
 
   validates :country, length: { is: 3 }
   validate :country_is_supported

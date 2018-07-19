9d6b3e17976931abe01e01785d7d1da4ad80a6d6
Mark Ottaviani <mark.ottaviani@gmail.com>
2018-07-19 19:09:05 -0400

    Revert "fixes #852 Removes dog-status-definition page"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,12 @@
 #    limitations under the License.
 
 class PagesController < ApplicationController
+  before_action :authenticate, only: [:status_definitions]
+
+  def status_definitions
+    @hide_topbar = true
+  end
+
   def dog_status_definitions
     @hide_topbar = true
   end

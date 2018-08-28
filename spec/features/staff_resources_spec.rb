86a979ca85b2ac555bef082d87dbce1f0ccd749c
Les Nightingill <codehacker@comcast.net>
2018-08-28 07:19:44 -0700

    wip on edit_resources

--- spec/features/staff_resources_spec.rb
+++ spec/features/staff_resources_spec.rb
@@ -169,4 +169,61 @@ feature "folder management", js: true do
       end
     end
   end
+
+  describe "show a folder" do
+    let(:admin){ create(:user, :admin) }
+    let(:folder){ create(:folder, :locked) }
+    let!(:resource){ create(:attachment, updated_by_user_id: admin.id, attachable_id: folder.id, attachable_type: 'Folder') }
+
+    before do
+      sign_in(admin)
+    end
+
+    it "should show list of attachments" do
+      visit folder_path(folder)
+      expect(page).to have_selector('.file_name', text: resource.attachment_file_name)
+      expect(page).to have_selector('.attachment input', text: 'Delete')
+    end
+  end
+
+  describe "search for attachments" do
+    let(:admin){ create(:user, :admin) }
+    let(:folder){ create(:folder, :locked) }
+    let!(:resource1){ create(:attachment, attachment_file_name: 'foo_file.pdf', updated_by_user_id: admin.id, attachable_id: folder.id, attachable_type: 'Folder') }
+    let!(:resource2){ create(:attachment, attachment_file_name: 'bar_file.pdf', updated_by_user_id: admin.id, attachable_id: folder.id, attachable_type: 'Folder') }
+
+    before do
+      sign_in(admin)
+    end
+
+    it "should show matching search results" do
+      # attachments should not have delete button here
+      visit folders_path
+      expect(page).to have_selector('#folders .folder', count: 1)
+      fill_in('search', with: 'bar')
+      page.find('#fetch_files').click
+      expect(page).to have_selector('.attachment', count: 1)
+      expect(page).to have_selector('.attachment .file_name', text: 'bar_file.pdf')
+      expect(page).not_to have_selector('.attachment input', text: 'Delete')
+      expect(1).to eq 0
+    end
+  end
+
+  describe "edit a resource" do
+    let(:folder){ create(:folder, :locked) }
+    let!(:resource){ create(:attachment, attachable_id: folder.id) }
+
+    describe "when user is admin" do
+      before do
+        visit folders_path(folder)
+      end
+
+      it "should save edited description" do
+        click_link('Edit')
+      end
+    end
+
+    describe "when user is not admin" do
+    end
+  end
 end

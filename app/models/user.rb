505bc4b09e6d212c5f732734e4594206f26179fb
Shaun Butler <shaun.butler@gmail.com>
2018-08-31 22:25:33 -0400

    Removes usermailer and calling code

--- app/models/user.rb
+++ app/models/user.rb
@@ -184,13 +184,6 @@ class User < ApplicationRecord
     lastverified.blank? || (lastverified.to_date < 30.days.ago.to_date)
   end
 
-  def send_password_reset
-    generate_token(:password_reset_token)
-    self.password_reset_sent_at = Time.zone.now
-    save!(validate: false)
-    UserMailer.password_reset(self).deliver_later
-  end
-
   def chimp_subscribe
     UserSubscribeJob.perform_later(name, email)
   end

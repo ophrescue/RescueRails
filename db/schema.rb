63bc612b9f529bb6c1a0999dd7eab9c603c72b0f
Shaun Butler <shaun.butler@gmail.com>
2018-09-08 13:35:26 -0400

    Reverts to master db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_08_004040) do
+ActiveRecord::Schema.define(version: 2018_06_06_150422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

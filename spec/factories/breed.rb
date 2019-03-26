543aa39f267fbec8e04777ba554e007151d51446
Mark Ottaviani <mark.ottaviani@gmail.com>
2019-03-26 17:02:59 -0400

    Faker deprication fixes

--- spec/factories/breed.rb
+++ spec/factories/breed.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :breed do
-    name { 
-      until(bb = Faker::Dog.breed; !Breed.pluck(:name).include?(bb))
+    name {
+      until(bb = Faker::Creature::Dog.breed; !Breed.pluck(:name).include?(bb))
       end
       bb
     }

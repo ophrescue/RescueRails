5648cef54303c296ce4ffdff89d9e4e25c1f6230
Mark Ottaviani <mark.ottaviani@gmail.com>
2019-04-06 18:39:30 -0400

    shine pass

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 feature 'Dashboard', js: true do
-  scenario  'visit dashboard as volunteer' do
-
+  scenario 'visit dashboard as volunteer' do
     sign_in_as_admin
     visit dashboards_path
     expect(page).to have_content 'Operation Paws for Homes'
-    expect(page).to have_content 'Nearby Events'
+    expect(page).to have_content 'Upcoming Events'
     expect(page).to have_content 'My Dogs'
     expect(page).to have_content 'My Adopters'
   end

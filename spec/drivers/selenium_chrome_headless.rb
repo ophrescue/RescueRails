1a5af6e18e3c7450deaafbf0257e8dae4f6f182d
Mark Ottaviani <mark.ottaviani@gmail.com>
2019-07-04 14:15:07 -0400

    more travis troubleshooting

--- spec/drivers/selenium_chrome_headless.rb
+++ spec/drivers/selenium_chrome_headless.rb
@@ -1,6 +1,6 @@
 Capybara.register_driver :selenium_chrome_headless do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless no-sandbox disable-gpu window-size=1366,2000] }
+    chromeOptions: { args: %w[headless remote-debugging-port=9222 disable-gpu window-size=1366,2000] }
   )
   Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
 end

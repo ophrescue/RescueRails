c0d84c9c0aab3ac12b2d95060b056b763a252e74
Mark Ottaviani <mark.ottaviani@gmail.com>
2019-07-04 13:21:26 -0400

    remove invalid chrome version var

--- spec/drivers/selenium_chrome_headless.rb
+++ spec/drivers/selenium_chrome_headless.rb
@@ -1,5 +1,4 @@
 Capybara.register_driver :selenium_chrome_headless do |app|
-  Webdrivers::Chromedriver.version = '2.40'
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: { args: %w[headless disable-gpu window-size=1366,2000] }
   )

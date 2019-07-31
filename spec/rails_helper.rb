40079741fce92cd77f537e0090fc1eea5e3f7ff4
Mark Ottaviani <mark.ottaviani@gmail.com>
2019-07-30 20:27:01 -0400

    fixes response in request specs https://stackoverflow.com/a/53643581/6608616

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,7 +52,6 @@ RSpec.configure do |config|
   config.include LoginMacros
   config.include ActiveSupport::Testing::TimeHelpers
   config.include FactoryBot::Syntax::Methods
-  config.include Rack::Test::Methods, type: :request
 
   # Disable Rails transactional fixtures in favor of DatabaseCleaner
   config.use_transactional_fixtures = false

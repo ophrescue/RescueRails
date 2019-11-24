e8d6088f7cac63ad8d0e7836695ee1815a7e328d
Mark Ottaviani <mark.ottaviani@gmail.com>
2019-11-24 11:25:40 -0500

    WIP support for cats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_202610) do
+ActiveRecord::Schema.define(version: 2019_11_14_131011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -172,6 +172,64 @@ ActiveRecord::Schema.define(version: 2019_10_21_202610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "active", default: true
+  end
+
+  create_table "cat_breeds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.string "original_name"
+    t.integer "tracking_id"
+    t.integer "primary_breed_id"
+    t.integer "secondary_breed_id"
+    t.string "status"
+    t.string "age", limit: 75
+    t.string "size", limit: 75
+    t.boolean "is_altered"
+    t.string "gender", limit: 6
+    t.boolean "declawed"
+    t.boolean "litter_box_trained"
+    t.string "coat_length"
+    t.boolean "is_special_needs"
+    t.boolean "no_dogs"
+    t.boolean "no_cats"
+    t.boolean "no_kids"
+    t.text "description"
+    t.integer "foster_id"
+    t.date "adoption_date"
+    t.boolean "is_uptodateonshots", default: true
+    t.date "intake_dt"
+    t.date "available_on_dt"
+    t.boolean "has_medical_need", default: false
+    t.boolean "is_high_priority", default: false
+    t.boolean "needs_photos", default: false
+    t.boolean "has_behavior_problem", default: false
+    t.boolean "needs_foster", default: false
+    t.string "petfinder_ad_url"
+    t.string "craigslist_ad_url"
+    t.string "youtube_video_url"
+    t.string "microchip"
+    t.integer "fee"
+    t.integer "coordinator_id"
+    t.string "sponsored_by"
+    t.integer "shelter_id"
+    t.text "medical_summary"
+    t.text "behavior_summary"
+    t.boolean "medical_review_complete", default: false
+    t.string "first_shots"
+    t.string "second_shots"
+    t.string "third_shots"
+    t.string "rabies"
+    t.string "felv_fiv_test"
+    t.string "flea_tick_preventative"
+    t.string "dewormer"
+    t.string "coccidia_treatment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", id: :serial, force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,31 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.destroy_all
+# User.destroy_all
 
-@attr = { name: "Admin User",
-          email: "test3@test.com",
-          password: "foobar99",
-          password_confirmation: "foobar99",
-          region: 'NY',
-          country: 'USA'
-}
+# @attr = { name: "Admin User",
+#           email: "test3@test.com",
+#           password: "foobar99",
+#           password_confirmation: "foobar99",
+#           region: 'NY',
+#           country: 'USA'
+# }
 
-user = User.create!(@attr)
-user.toggle!(:admin)
+# user = User.create!(@attr)
+# user.toggle!(:admin)
+
+# Breed.destroy_all
+# open("db/dog_breeds.txt") do |breeds|
+#   breeds.read.each_line do |breed|
+#     breed = breed.chomp
+#     Breed.create!(name: breed)
+#   end
+# end
+
+CatBreed.destroy_all
+open("db/cat_breeds.txt") do |breeds|
+  breeds.read.each_line do |breed|
+    breed = breed.chomp
+    CatBreed.create!(name: breed)
+  end
+end

779b1b9a895f5eb37cc30948a96895fcaa3613d0
Mark Ottaviani <mark.ottaviani@gmail.com>
2020-02-07 21:03:26 -0500

    closes #1404 bite prevention email (#1405)

---
+++ app/mailers/bite_prevention_mailer.rb
@@ -0,0 +1,27 @@
+#    Copyright 2020 Operation Paws for Homes
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+class BitePreventionMailer < ActionMailer::Base
+  default from: "Operation Paws for Homes <adopt@ophrescue.org>",
+          return_path: 'adopt@ophrescue.org',
+          reply_to: 'adopt@ophrescue.org'
+
+  def bite_prevent(adopter_id)
+    @adopter = Adopter.find(adopter_id)
+    mail(to: "#{@adopter.email}",
+         reply_to: 'adopt@ophrescue.org',
+         subject: "Dog Bite Prevention - Please Respond",
+         content_type: 'text/html')
+  end
+end

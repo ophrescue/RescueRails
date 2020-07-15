4f7bb5e6e9f36e5081cddded7cf25a9069041200
Mark Ottaviani <mark.ottaviani@gmail.com>
2020-07-15 14:31:12 -0400

    fix type-o

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 Rails.application.config.session_store :cookie_store,
                                         key: '_RescueRails_session',
-                                        :expire_after: 2.weeks
+                                        expire_after: 2.weeks

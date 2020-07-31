449dffad51c504e5679f2765664ec32183e8fc30
Mark Ottaviani <mark.ottaviani@gmail.com>
2020-07-31 16:10:00 -0400

    removes household ages from schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_07_13_164249) do
-  
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,7 +84,6 @@ ActiveRecord::Schema.define(version: 2020_07_13_164249) do
     t.date "birth_date"
     t.string "prev_pets_type"
     t.string "current_pets_type"
-    t.text "household_ages", default: [], array: true
     t.string "building_type"
     t.boolean "fenced_yard"
     t.index ["adopter_id"], name: "index_adoption_apps_on_adopter_id"

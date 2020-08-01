a5b5e4b37f9602a170a9694a2d45f40a37c6ae32
Sarah Buckingham <sarah@crisistextline.org>
2020-07-31 23:25:09 -0700

    reverting back to original migration

--- db/migrate/20200731175918_add_has_kids_foster_data_to_users.rb
+++ db/migrate/20200731175918_add_has_kids_foster_data_to_users.rb
@@ -1,11 +1,5 @@
 class AddHasKidsFosterDataToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :has_children, :boolean, default: false, null: false
-    query = <<-SQL
-     update users
-     set has_children = children_under_five
-   SQL
-
-   execute query
   end
 end

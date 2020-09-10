56b2d958d8421f58de096ed06306d05e6ca2a3b7
Mark Ottaviani <mark.ottaviani@gmail.com>
2020-09-10 17:49:10 -0400

    add microchip registration page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ STATIC_PAGES = ['5k', 'contact', 'funding-partners', 'community-partners', 'non-
                 'training-partners', 'guide', 'aboutus', 'documentary', 'insurance',
                 'get-involved', 'volunteer', 'foster', 'fosterfaq', 'donate', 'sponsor', 'newsletters',
                 'special-funds', 'other-ways-to-give', 'terms', 'resources', 'tips-for-finding-lost-pets', 'status_definitions',
-                'education-and-outreach','adoption-fee-payments'].freeze
+                'education-and-outreach','adoption-fee-payments','microchip-registration'].freeze
 
 RescueRails::Application.routes.draw do
   get "/adopters/check_email", to: "adopters#check_email"

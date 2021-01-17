487b6d6847655f8d26c18e6254a6f6619598df7a
Mark Ottaviani <mark.ottaviani@gmail.com>
2021-01-17 13:16:11 -0500

    coverage fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,16 @@ require 'faker'
 
 Faker::Config.random = Random.new(RSpec.configuration.seed)
 
+# Fix incompatibility of simplecov-lcov with older versions of simplecov that are not expresses in its gemspec.
+# https://github.com/fortissimo1997/simplecov-lcov/pull/25
+if !SimpleCov.respond_to?(:branch_coverage)
+  module SimpleCov
+    def self.branch_coverage?
+      false
+    end
+  end
+end
+
 SimpleCov::Formatter::LcovFormatter.config do |c|
   c.report_with_single_file = true
   c.single_report_path = 'coverage/lcov.info'
@@ -15,7 +25,9 @@ SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
     SimpleCov::Formatter::LcovFormatter,
   ]
 )
-SimpleCov.start('rails')
+SimpleCov.start('rails') do
+  add_filter 'spec/'
+end
 
 RSpec.configure do |config|
   # Use documentation formatter when running a single file.

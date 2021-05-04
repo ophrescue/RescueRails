dde8939b1f8a29cd504302d606f7942d6796f340
Mark Ottaviani <mark.ottaviani@gmail.com>
2021-05-04 19:49:29 -0400

    remove debug statements

--- spec/requests/adopters_request_spec.rb
+++ spec/requests/adopters_request_spec.rb
@@ -5,14 +5,12 @@ describe "Adoption Application Creation", type: :request do
   it "Can Not Create Duplicate application" do
 
     duplicate_adopter = attributes_for(:adopter, :with_app, email: first_adopter.email)
-    puts duplicate_adopter
     expect {
       post adopters_path, params: { adopter: duplicate_adopter }
     }.to change(Adopter, :count).by(0)
   end
   it "Will not allow duplicate email in different case" do
     duplicate_adopter_case = attributes_for(:adopter, :with_app, email: first_adopter.email.upcase)
-    puts duplicate_adopter_case
     expect {
       post adopters_path, params: { adopter: duplicate_adopter_case }
     }.to change(Adopter, :count).by(0)

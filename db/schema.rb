47ba808e9888c5b216dd1f5c70a430cb020d439f
Mark Ottaviani <mark.ottaviani@gmail.com>
2021-07-24 11:48:32 -0400

    add minors for volunteerApp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_202044) do
+ActiveRecord::Schema.define(version: 2021_07_24_153630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -651,6 +651,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_202044) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "marketing_interest"
+    t.boolean "adult"
   end
 
   create_table "volunteer_foster_apps", force: :cascade do |t|

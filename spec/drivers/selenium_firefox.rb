14e786163476e757b7237c3156b438bc116747cd
Mark Ottaviani <mark.ottaviani@gmail.com>
2023-08-26 11:54:06 -0400

    driver cleanup

--- spec/drivers/selenium_firefox.rb
+++
@@ -1,8 +0,0 @@
-Capybara.register_driver :selenium_firefox do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
-  Capybara::Selenium::Driver.new(app, :browser => :firefox, :options => options)
-end
-
-Capybara::Screenshot.register_driver(:selenium_firefox) do |driver, path|
-  driver.browser.save_screenshot(path)
-end

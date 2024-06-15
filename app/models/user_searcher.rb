16c45fee767b1afcf560bf45e7202904d1426495
Mark Ottaviani <mark.ottaviani@gmail.com>
2024-06-15 12:52:47 -0400

    cat and dog foster mentors

--- app/models/user_searcher.rb
+++ app/models/user_searcher.rb
@@ -56,7 +56,7 @@ class UserSearcher
 
   def filtering_params
     @params.slice(:admin, :adoption_coordinator, :event_planner,
-                 :dog_adder, :dog_editor, :photographer, :foster,
+                 :dog_adder, :dog_editor, :photographer, :foster, :cat_handling_training_complete, :dog_handling_training_complete,
                  :newsletter, :clerical, :has_dogs, :has_cats, :house_type, :has_children, :has_children_under_five,
                  :has_fence, :puppies_ok, :dogs_ok, :cats_ok, :has_parvo_house, :transporter, :training_team,
                  :foster_mentor, :translator, :public_relations, :fundraising, :medical_behavior,

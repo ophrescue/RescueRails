78870c179e902bb592d59e2e13efe83c4ea5e9bb
Mark Ottaviani <mark.ottaviani@gmail.com>
2025-06-03 20:21:05 -0400

    remove test

--- spec/requests/volunteer_app_request_spec.rb
+++ spec/requests/volunteer_app_request_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "VolunteerApps", type: :request do
+RSpec.xdescribe "VolunteerApps", type: :request do
   context 'pubic applications' do
 
-    describe 'GET #new' do
+    xdescribe 'GET #new' do
       it 'is successful' do
         get new_volunteer_app_path()
         expect(response).to be_successful
       end
     end
 
-    describe "POST #create" do
+    xdescribe "POST #create" do
       it "is able to create a volunteer application" do
         volunteer_app = attributes_for(:volunteer_app)
         expect {
